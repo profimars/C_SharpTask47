add3d12c53c7b42c3553f00cd29c16b09bd4a97c
Марс <profimars@mail.ru>
2023-03-22 21:02:24 +0300

    добавил задачу C_SharpTask47

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+//https://github.com/profimars/C_SharpTask47
+
+void CreateArray(double[,] A)
+{
+    int m = A.GetLength(0);
+    int n = A.GetLength(1);
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            A[i, j] = Math.Round(rnd.NextDouble(), 1);
+        }
+}
+
+void PrintArray(double[,] A)
+{
+    int m = A.GetLength(0);
+    int n = A.GetLength(1);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            Console.Write($"{A[i, j],3}    ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк матрицы m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы n");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] A = new double[m, n];
+CreateArray(A);
+PrintArray(A);
